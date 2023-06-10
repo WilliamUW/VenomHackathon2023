ddeb4f1baaea7b31ae2fcc215859bd22259c0a27
William Wang <bn.william.w@gmail.com>
2023-06-10 16:36:39 -0400

    Working token args

--- venom-token-creation/cmdAPI.py
+++ venom-token-creation/cmdAPI.py
@@ -1,12 +1,17 @@
 import subprocess
 
+tokenName = "TokenName"
+tokenSymbol = "TokenSymbol"
+tokenInitialSupply = 123
+tokenInitialSupplyParameter = tokenInitialSupply * 1000000000
+
 # Define the command you want to run
 nftCommand = "npx locklift run -s ./scripts/1-deploy-sample.ts -n local"
-tokenCommandLocal = "npx locklift run -s ./scripts/1-deploy-token-root.ts \"VenomChat Token\" \"VCHAT\" 100000000000 -n local"
+tokenCommandLocal = f"npx locklift run -s ./scripts/1-deploy-token-root.ts \"{tokenName}\" \"{tokenSymbol}\" {tokenInitialSupplyParameter} -n local"
 tokenCommandTestnet = "npx locklift run -s ./scripts/1-deploy-token-root.ts \"Testnet Python\" \"TP\" 100000000000 -n venom_testnet"
 
 # Run the command and capture the output
-output = subprocess.check_output(tokenCommandTestnet, shell=True)
+output = subprocess.check_output(tokenCommandLocal, shell=True)
 
 # Decode the output (assuming it's in UTF-8 encoding)
 decoded_output = output.decode("utf-8")
